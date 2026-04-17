65ae2aca0058915ad84f05471572f7f36257a3bc
Anna Lifanova <50072172+Lifanovaa25@users.noreply.github.com>
2026-04-17 11:55:55 +0300

    fix mobile styles

--- src/widgets/StudentsTable/StudentsTable.tsx
+++ src/widgets/StudentsTable/StudentsTable.tsx
@@ -188,20 +188,16 @@ export default function StudentsTable({
                                                     <span className={styles.paidIcon}>
                                                         <svg
                                                             viewBox="0 0 24 24"
-                                                            width="16"
-                                                            height="16"
-                                                            fill="none"
+                                                            className={styles.checkSvg}
                                                             xmlns="http://www.w3.org/2000/svg"
                                                         >
                                                             <path
+                                                                className={styles.checkPath}
                                                                 d="M20 6L9 17L4 12"
-                                                                stroke="currentColor"
-                                                                strokeWidth="2.5"
-                                                                strokeLinecap="round"
-                                                                strokeLinejoin="round"
                                                             />
                                                         </svg>
                                                     </span>
+
                                                     <span className={styles.paidText}>оплачено</span>
                                                 </div>
 
